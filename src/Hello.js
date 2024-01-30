e81f235d4dd64cdc7ac36c7108f835ce289d47f3
Rory Wood <rorywood9@live.com>
2024-01-30 11:55:31 -0500

    added hello component

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+
+const Hello = ({greeting}) => {
+  return (
+    <div>
+      <h1 className='tc'>Hello There!</h1>
+      <p>{greeting}</p>
+      <p>Wlecome to mobile app develompent</p>
+    </div>
+  );
+};
+
+export default Hello;
