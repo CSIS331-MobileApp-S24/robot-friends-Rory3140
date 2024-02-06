dda2f94dacd265b8c63869ba29e45fac9ebee04c
Rory Wood <rorywood9@live.com>
2024-02-06 12:20:42 -0500

    end of third class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from "react";
+import SearchBox from "./SearchBox";
 import CardList from "./CardList";
 import { robots } from "./robots";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Robot Friends</h1>
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { robots: robots, Searchfield: "" };
+  }
+
+  onSearchChange = (event) => {
+    var input = event.target.value;
+    input = input.toLowerCase();
+    this.setState({Searchfield: input});
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.Searchfield);
+    });
+
+    return (
+      <div className="tc">
+        <h1>Robot Friends</h1>
+        <SearchBox searchChange={this.onSearchChange}></SearchBox>
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
